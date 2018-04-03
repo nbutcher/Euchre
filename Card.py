b71b7d8b644d6d54e48926bdca3246912be9b01f
Nathan <butchernate@gmail.com>
2018-04-03 16:19:00 -0700

    Started Card class and have a file to quickly test functionality

---
+++ Card.py
@@ -0,0 +1,21 @@
+class Card:
+    def __init__(self, rank, suit):
+        self.r = rank
+        self.s = suit
+
+    def show(self):
+        if (self.r < 10):
+            outr = ' ' + str(self.r)
+        elif (self.r == 10):
+            outr = str(self.r)
+        elif (self.r == 11):
+            outr = ' J'
+        elif (self.r == 12):
+            outr = ' Q'
+        elif (self.r == 13):
+            outr = ' K'
+        elif (self.r == 14):
+            outr = ' A'
+        else:
+            print("Rank Error")
+        print(outr + self.s)
---
+++ test_driver.py
@@ -0,0 +1,9 @@
+import Card
+
+a = Card.Card(2,'C')
+b = Card.Card(12,'S')
+c = Card.Card(10,'H')
+
+a.show()
+b.show()
+c.show()
